f0027224ac964c902aca71f518ae100828e8cce7
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-21 22:20:43 -0400

    import BrowserRouter as Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./components/Navbar";
 import Activities from "./components/Activities";
