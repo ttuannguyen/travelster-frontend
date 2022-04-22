b3c900663f4a3c197a1337deb7e74927cfde67de
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-21 21:53:08 -0400

    organize components in a new folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>My App</h1>
     </div>
   );
 }
