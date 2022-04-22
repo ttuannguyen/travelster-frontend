a8c249fa06674f84e913e00d96ec456f6fc28a91
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-22 12:18:16 -0400

    revise the content of the form and declare a variable to store the keys and values for the inputs and pass it as value for the body key in the fetch call

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Activity = ({activity}) => {
   return (
     <div>
-      <h3>Name: {activity.name}</h3>
+      <h3>{activity.activity}</h3>
       <h4>Trip: {activity.trip}</h4>
       <p>Likes: {activity.likes}</p>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,12 +8,14 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    let obj = { activity: e.target.name.value }
+
     fetch(url, {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({activity: e.target.name.value}),
+      body: JSON.stringify(obj),
     })
     .then(res => res.json())
     .then(json => console.log(json))
@@ -28,7 +30,13 @@ const Form = () => {
       <h2>Add an activity</h2>
       <form onSubmit={handleSubmit}>
         <label>Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' /><br />
+        <label>Trip</label>
+        <input type='text' id='trip' /><br />
+        <label>Location</label>
+        <input type='text' id='location' /><br />
+        <label>Comments</label>
+        <input type='text' id='comments' /><br />
         <input type='submit'/>
       </form>
     </div>
