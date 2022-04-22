33bf88d123501dfd169d767917fa6e16aa04eb63
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-21 22:05:16 -0400

    import the components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Home from "./Home";
+import Navbar from "./components/Navbar";
+import Activities from "./components/Activities";
+import Activity from "./components/Activity";
 
 const App = () => {
   return (
