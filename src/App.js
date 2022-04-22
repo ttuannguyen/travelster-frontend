fae4e647fb49080961f7b01af6375d8bb6c25afe
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-21 23:42:52 -0400

    import useState and useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./components/Navbar";
