3b6f7add87f706a19cce7bc1cbfc9d8987e9d24e
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-04-23 19:46:45 -0400

    add onChange event listeners and handlers to control the state of the inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,23 @@ const Form = () => {
   const [trip, setTrip] = useState('');
   const [location, setLocation] = useState('');
   const [comment, setComment] = useState('');
+
+  const handleName = (e) => {
+    //console.log(e.target.value)
+    setName(e.target.value)
+  }
+
+  const handleTrip = (e) => {
+    setTrip(e.target.value)
+  }
+
+  const handleLocation = (e) => {
+    setLocation(e.target.value)
+  }
+
+  const handleComment = (e) => {
+    setComment(e.target.value)
+  }
    
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,24 +46,19 @@ const Form = () => {
 
   }
 
-  const handleName = (e) => {
-    console.log(e.target.value)
-  }
-
-
 
   return (
     <div>
       <h2>Add an activity</h2>
       <form onSubmit={handleSubmit}>
         <label>Activity Name</label>
-        <input type='text' id='name' name='name' /><br />
+        <input type='text' id='name' name='name' value={name} onChange={handleName} /><br />
         <label>Trip</label>
-        <input type='text' id='trip' name='trip' /><br />
+        <input type='text' id='trip' name='trip' value={trip} onChange={handleTrip} /><br />
         <label>Location</label>
-        <input type='text' id='location' name='location' /><br />
+        <input type='text' id='location' name='location' value={location} onChange={handleLocation} /><br />
         <label>Comment</label>
-        <textarea id='comment' name='comment' /><br />
+        <textarea id='comment' name='comment' value={comment} onChange={handleComment} /><br />
         <input type='submit'/>
       </form>
     </div>
