0045417122d830779289ef33900ece8486eb5f90
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-05-16 17:11:40 -0400

    update styling of nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
       <div id="nav-bar">
-          <NavLink className="navlink" style={{ marginRight: "20px", textDecoration: 'none' }} to="/"> Home </NavLink>
-          <NavLink className="navlink" style={{ marginRight: "20px", textDecoration: 'none' }} to="/activities"> Activities </NavLink>
-          <NavLink className="navlink" style={{ marginRight: "20px", textDecoration: 'none' }} to="/activities/new"> Add a new activity! </NavLink>
+          <NavLink className="navlink" style={{ marginRight: "20px"}} to="/"> Home </NavLink>
+          <NavLink className="navlink" style={{ marginRight: "20px"}} to="/activities"> Activities </NavLink>
+          <NavLink className="navlink" style={{ marginRight: "20px"}} to="/activities/new"> Add a new activity! </NavLink>
       </div>
   )
 }
