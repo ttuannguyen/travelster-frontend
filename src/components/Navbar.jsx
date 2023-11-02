bbf4f3082697acd6ec031688ff40333d64eb4a0f
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-11-01 22:07:59 -0400

    add cover photo for the Travelster project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div id="nav-bar">
           <NavLink className="navlink" style={linkStyles} to="/"> Home </NavLink>
           <NavLink className="navlink" style={linkStyles} to="/activities"> Activities </NavLink>
-          <NavLink className="navlink" style={linkStyles} to="/activities/new"> Add a new activity! </NavLink>
+          <NavLink className="navlink" style={linkStyles} to="/activities/new"> Add Activity </NavLink>
       </div>
   )
 }
---
+++ test.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3]
+const newArr = [...arr, 4]
+console.log(newArr)
+
+
+const obj = { name: 'John'}
+const newObj = { ...obj, age: 31 }
+console.log(newObj)
